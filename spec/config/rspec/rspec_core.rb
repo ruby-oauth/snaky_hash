2b29a46965f714e8581c2d3b741de9ddd71a0344
Peter Boling <peter.boling@gmail.com>
2022-08-27 04:01:58 +0700

    ♻️ transpec'd

--- spec/config/rspec/rspec_core.rb
+++ spec/config/rspec/rspec_core.rb
@@ -5,9 +5,9 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
-  # config.disable_monkey_patching!
+  config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
-    c.syntax = :should
+    c.syntax = :expect
   end
 end
--- spec/snaky_hash/snake_spec.rb
+++ spec/snaky_hash/snake_spec.rb
@@ -28,43 +28,43 @@ RSpec.describe SnakyHash::Snake do
     )
   end
 
-  # it { is_expected.to be_a(Hashie::Mash) }
+  it { is_expected.to be_a(Hashie::Mash) }
 
   it "creates a new rash where all the keys are underscored instead of camelcased" do
-    subject.var_one.should == 1
-    subject.two.should == 2
-    subject.three.should == 3
-    subject.var_four.should == 4
-    subject.five_hump_humps.should == 5
-    subject.nested.should be_a(described_class)
-    subject.nested.nested_one.should == "One"
-    subject.nested.two.should == "two"
-    subject.nested.nested_three.should == "three"
-    subject.nested_two.should be_a(described_class)
-    subject.nested_two.nested_two.should == 22
-    subject.nested_two.nested_three.should == 23
-    subject.spaced_key.should == "When would this happen?"
-    subject.trailing_spaces.should == "better safe than sorry"
-    subject.extra_spaces.should == "hopefully this never happens"
+    expect(subject.var_one).to eq(1)
+    expect(subject.two).to eq(2)
+    expect(subject.three).to eq(3)
+    expect(subject.var_four).to eq(4)
+    expect(subject.five_hump_humps).to eq(5)
+    expect(subject.nested).to be_a(described_class)
+    expect(subject.nested.nested_one).to eq("One")
+    expect(subject.nested.two).to eq("two")
+    expect(subject.nested.nested_three).to eq("three")
+    expect(subject.nested_two).to be_a(described_class)
+    expect(subject.nested_two.nested_two).to eq(22)
+    expect(subject.nested_two.nested_three).to eq(23)
+    expect(subject.spaced_key).to eq("When would this happen?")
+    expect(subject.trailing_spaces).to eq("better safe than sorry")
+    expect(subject.extra_spaces).to eq("hopefully this never happens")
   end
 
   it "allows camelCased accessors" do
     # avoiding hashie v5- warnings
     subject.class.disable_warnings(:varOne) if Gem::Version.new(Hashie::VERSION) >= Gem::Version.new("5.0.0")
 
-    subject.varOne.should == 1
+    expect(subject.varOne).to eq(1)
     subject.varOne = "once"
-    subject.varOne.should == "once"
-    subject.var_one.should == "once"
+    expect(subject.varOne).to eq("once")
+    expect(subject.var_one).to eq("once")
   end
 
   it "allows camelCased accessors on nested hashes" do
     # avoiding hashie v5-  warnings
     subject.class.disable_warnings(:nestedOne) if Gem::Version.new(Hashie::VERSION) >= Gem::Version.new("5.0.0")
 
-    subject.nested.nestedOne.should == "One"
+    expect(subject.nested.nestedOne).to eq("One")
     subject.nested.nestedOne = "once"
-    subject.nested.nested_one.should == "once"
+    expect(subject.nested.nested_one).to eq("once")
   end
 
   it "merges well with a Mash" do
@@ -73,12 +73,12 @@ RSpec.describe SnakyHash::Snake do
       nested3: { helloWorld: "hi" }
     )
 
-    merged.nested.four_times.should == "a charm"
-    merged.nested.fourTimes.should == "a charm"
-    merged.nested3.should be_a(described_class)
-    merged.nested3.hello_world.should == "hi"
-    merged.nested3.helloWorld.should == "hi"
-    merged[:nested3][:helloWorld].should == "hi"
+    expect(merged.nested.four_times).to eq("a charm")
+    expect(merged.nested.fourTimes).to eq("a charm")
+    expect(merged.nested3).to be_a(described_class)
+    expect(merged.nested3.hello_world).to eq("hi")
+    expect(merged.nested3.helloWorld).to eq("hi")
+    expect(merged[:nested3][:helloWorld]).to eq("hi")
   end
 
   it "updates well with a Mash" do
@@ -87,12 +87,12 @@ RSpec.describe SnakyHash::Snake do
       nested3: { helloWorld: "hi" }
     )
 
-    subject.nested.four_times.should == "a charm"
-    subject.nested.fourTimes.should == "a charm"
-    subject.nested3.should be_a(described_class)
-    subject.nested3.hello_world.should == "hi"
-    subject.nested3.helloWorld.should == "hi"
-    subject[:nested3][:helloWorld].should == "hi"
+    expect(subject.nested.four_times).to eq("a charm")
+    expect(subject.nested.fourTimes).to eq("a charm")
+    expect(subject.nested3).to be_a(described_class)
+    expect(subject.nested3.hello_world).to eq("hi")
+    expect(subject.nested3.helloWorld).to eq("hi")
+    expect(subject[:nested3][:helloWorld]).to eq("hi")
   end
 
   it "merges well with a Hash" do
@@ -101,42 +101,42 @@ RSpec.describe SnakyHash::Snake do
       nested3: { helloWorld: "hi" }
     )
 
-    merged.nested.four_times.should == "work like a charm"
-    merged.nested.fourTimes.should == "work like a charm"
-    merged.nested3.should be_a(described_class)
-    merged.nested3.hello_world.should == "hi"
-    merged.nested3.helloWorld.should == "hi"
-    merged[:nested3][:helloWorld].should == "hi"
+    expect(merged.nested.four_times).to eq("work like a charm")
+    expect(merged.nested.fourTimes).to eq("work like a charm")
+    expect(merged.nested3).to be_a(described_class)
+    expect(merged.nested3.hello_world).to eq("hi")
+    expect(merged.nested3.helloWorld).to eq("hi")
+    expect(merged[:nested3][:helloWorld]).to eq("hi")
   end
 
   it "handles assigning a new Hash and convert it to a rash" do
     subject.nested3 = { helloWorld: "hi" }
 
-    subject.nested3.should be_a(described_class)
-    subject.nested3.hello_world.should == "hi"
-    subject.nested3.helloWorld.should == "hi"
-    subject[:nested3][:helloWorld].should == "hi"
+    expect(subject.nested3).to be_a(described_class)
+    expect(subject.nested3.hello_world).to eq("hi")
+    expect(subject.nested3.helloWorld).to eq("hi")
+    expect(subject[:nested3][:helloWorld]).to eq("hi")
   end
 
   it "converts an array of Hashes" do
-    subject.nested_three.should be_a(Array)
-    subject.nested_three[0].should be_a(described_class)
-    subject.nested_three[0].nested_four.should == 4
-    subject.nested_three[1].should be_a(described_class)
-    subject.nested_three[1].nested_four.should == 4
+    expect(subject.nested_three).to be_a(Array)
+    expect(subject.nested_three[0]).to be_a(described_class)
+    expect(subject.nested_three[0].nested_four).to eq(4)
+    expect(subject.nested_three[1]).to be_a(described_class)
+    expect(subject.nested_three[1].nested_four).to eq(4)
   end
 
   it "allows initializing reader" do
     subject.nested3!.helloWorld = "hi"
-    subject.nested3.hello_world.should == "hi"
+    expect(subject.nested3.hello_world).to eq("hi")
   end
 
   it "does not transform non-Symbolizable keys" do
-    subject[4].should == "not symbolizable"
-    subject[:"4"].should be_nil
-    subject["4"].should be_nil
-    subject.key?("4").should be false
-    subject.key?(:"4").should be false
-    subject.key?(4).should be true
+    expect(subject[4]).to eq("not symbolizable")
+    expect(subject[:"4"]).to be_nil
+    expect(subject["4"]).to be_nil
+    expect(subject.key?("4")).to be false
+    expect(subject.key?(:"4")).to be false
+    expect(subject.key?(4)).to be true
   end
 end
