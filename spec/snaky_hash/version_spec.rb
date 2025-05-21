7187fcb340e46dcf972bcd465ca2ef3158a68051
Peter H. Boling <peter.boling@gmail.com>
2025-05-21 07:49:56 +0700

    💚 Fix CI

--- spec/snaky_hash/version_spec.rb
+++ spec/snaky_hash/version_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe SnakyHash::Version do
-  it "has a version number" do
-    expect(described_class::VERSION).not_to be_nil
+  it_behaves_like "a Version module", described_class
+
+  it "is greater than 1.0.0" do
+    expect(Gem::Version.new(described_class) >= Gem::Version.new("1.0.0")).to(be(true))
   end
 end
