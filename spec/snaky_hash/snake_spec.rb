c63d03d172c83186318701535fce72795303994d
Peter H. Boling <peter.boling@gmail.com>
2025-05-22 03:35:20 +0700

    ✨ Serialization Extensions (iii)

--- spec/snaky_hash/snake_spec.rb
+++ spec/snaky_hash/snake_spec.rb
@@ -34,117 +34,78 @@ RSpec.describe SnakyHash::Snake do
   end
 
   context "when serializer: true" do
-    # Older versions of Ruby can't do Integer#to_sym,
-    #   which means snaky_hash on older rubies can't handle integer keys via new
-    if RUBY_VERSION >= "3.0"
-      context "when Ruby >= 3.0" do
-        let(:snaky_klass) do
-          klass = Class.new(Hashie::Mash) do
-            include SnakyHash::Snake.new(
-              key_type: :symbol, # default :string
-              serializer: true,   # default: false
-            )
-          end
-
-          klass.load_hash_extensions.add(:non_zero_keys_to_int) do |value|
-            if value.is_a?(Hash)
-              value.transform_keys do |key|
-                key_int = key.to_s.to_i
-                if key_int > 0
-                  key_int
-                else
-                  key
-                end
-              end
-            else
-              value
-            end
-          end
-
-          klass
-        end
-
-        let(:snake) { snaky_klass.new(1 => "a", 0 => 4, "VeryFineHat" => {3 => "v", 5 => 7, :very_fine_hat => "feathers"}) }
-        let(:dump) { snaky_klass.dump(snake) }
-        let(:hydrated) { snaky_klass.load(dump) }
-
-        it "can initialize" do
-          expect(snake).to eq({1 => "a", 0 => 4, :very_fine_hat => {3 => "v", 5 => 7, :very_fine_hat => "feathers"}})
-        end
-
-        it "can dump" do
-          expect(dump).to eq "{\"1\":\"a\",\"0\":4,\"very_fine_hat\":{\"3\":\"v\",\"5\":7,\"very_fine_hat\":\"feathers\"}}"
-        end
-
-        it "can load" do
-          expect(hydrated).to eq({1 => "a", :"0" => 4, :very_fine_hat => {3 => "v", 5 => 7, :very_fine_hat => "feathers"}})
-        end
-
-        it "can access keys" do
-          expect(hydrated["1"]).to be_nil
-          expect(hydrated[1]).to eq("a")
-          expect(hydrated["0"]).to eq(4)
-          expect(hydrated[0]).to be_nil
-          expect(hydrated.very_fine_hat).to eq({3 => "v", 5 => 7, :very_fine_hat => "feathers"})
-          expect(hydrated.very_fine_hat.very_fine_hat).to eq("feathers")
-          expect(hydrated.very_fine_hat[:very_fine_hat]).to eq("feathers")
-          expect(hydrated.very_fine_hat["very_fine_hat"]).to eq("feathers")
-        end
+    let(:snaky_klass) do
+      klass = Class.new(Hashie::Mash) do
+        include SnakyHash::Snake.new(
+          key_type: :symbol, # default :string
+          serializer: true,   # default: false
+        )
       end
-    else
-      context "when Ruby < 3.0" do
-        let(:snaky_klass) do
-          klass = Class.new(Hashie::Mash) do
-            include SnakyHash::Snake.new(
-              key_type: :symbol, # default :string
-              serializer: true,   # default: false
-            )
-          end
 
-          klass.load_hash_extensions.add(:non_zero_keys_to_int) do |value|
-            if value.is_a?(Hash)
-              value.keys.each do |key|
-                key_int = key.to_s.to_i
-                if key_int > 0
-                  ref = value.delete(key)
-                  value[key_int] = ref
-                end
-              end
+      klass.load_hash_extensions.add(:keys_are_based) do |value|
+        if value.is_a?(Hash)
+          value.keys.sort.each_with_index do |key, index|
+            key_int = key.to_s.to_i
+            ref = value.delete(key)
+            encoded_key = if key_int > 0
+              # See: https://idiosyncratic-ruby.com/4-what-the-pack.html#m0--base64-encoding-rfc-4648
+              "dog-#{index}"
             else
-              value
+              "cat-#{index}"
             end
+            value[encoded_key] = ref
           end
-
-          klass
         end
-
-        let(:snake) { snaky_klass.new("1" => "a", "0" => 4, "VeryFineHat" => {"3" => "v", "5" => 7, :very_fine_hat => "feathers"}) }
-        let(:dump) { snaky_klass.dump(snake) }
-        let(:hydrated) { snaky_klass.load(dump) }
-
-        it "can initialize" do
-          expect(snake).to eq({"1" => "a", "0" => 4, :very_fine_hat => {"3" => "v", "5" => 7, :very_fine_hat => "feathers"}})
-        end
-
-        it "can dump" do
-          expect(dump).to eq "{\"1\":\"a\",\"0\":4,\"very_fine_hat\":{\"3\":\"v\",\"5\":7,\"very_fine_hat\":\"feathers\"}}"
-        end
-
-        it "can load" do
-          expect(hydrated).to eq({1 => "a", :"0" => 4, :very_fine_hat => {3 => "v", 5 => 7, :very_fine_hat => "feathers"}})
-        end
-
-        it "can access keys" do
-          expect(hydrated["1"]).to be_nil
-          expect(hydrated[1]).to eq("a")
-          expect(hydrated["0"]).to eq(4)
-          expect(hydrated[0]).to be_nil
-          expect(hydrated.very_fine_hat).to eq({3 => "v", 5 => 7, :very_fine_hat => "feathers"})
-          expect(hydrated.very_fine_hat.very_fine_hat).to eq("feathers")
-          expect(hydrated.very_fine_hat[:very_fine_hat]).to eq("feathers")
-          expect(hydrated.very_fine_hat["very_fine_hat"]).to eq("feathers")
-        end
+        value
       end
+
+      klass
+    end
+
+    let(:snake) { snaky_klass.new("1" => "a", "0" => 4, "VeryFineHat" => {"3" => "v", "5" => 7, :very_fine_hat => "feathers"}) }
+    let(:dump) { snaky_klass.dump(snake) }
+    let(:hydrated) { snaky_klass.load(dump) }
+
+    it "can initialize" do
+      expect(snake).to eq({"0": 4, "1": "a", very_fine_hat: {"3": "v", "5": 7, very_fine_hat: "feathers"}})
+    end
+
+    it "can dump" do
+      expect(dump).to eq "{\"1\":\"a\",\"0\":4,\"very_fine_hat\":{\"3\":\"v\",\"5\":7,\"very_fine_hat\":\"feathers\"}}"
+    end
+
+    it "can load" do
+      expect(hydrated).to eq(
+        {
+          dog_1: "a",
+          cat_0: 4,
+          cat_2: {
+            dog_0: "v",
+            dog_1: 7,
+            cat_2: "feathers",
+          },
+        },
+      )
+    end
+
+    it "can access keys" do
+      expect(hydrated["1"]).to be_nil
+      expect(hydrated[1]).to be_nil
+      expect(hydrated["dog_1"]).to eq("a")
+      expect(hydrated[:dog_1]).to eq("a")
+      expect(hydrated.dog_1).to eq("a")
+      expect(hydrated["0"]).to be_nil
+      expect(hydrated[0]).to be_nil
+      expect(hydrated["cat_0"]).to eq(4)
+      expect(hydrated[:cat_0]).to eq(4)
+      expect(hydrated.cat_0).to eq(4)
+      expect(hydrated.very_fine_hat).to be_nil
+      expect(hydrated.cat_2).to eq({cat_2: "feathers", dog_0: "v", dog_1: 7})
+      expect(hydrated.cat_2.cat_2).to eq("feathers")
+      expect(hydrated.cat_2["cat_2"]).to eq("feathers")
+      expect(hydrated.cat_2[:cat_2]).to eq("feathers")
+      expect(hydrated.cat_2.dog_0).to eq("v")
+      expect(hydrated.cat_2.dog_1).to eq(7)
     end
   end
 end
